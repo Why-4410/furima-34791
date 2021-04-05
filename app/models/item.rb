30922b2e6f27335a53daa8fe2850abdd304c50b1
Why-4410 <yssy4410@gmail.com>
2021-04-05 22:05:17 +0900

    レビューの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,14 +20,16 @@ class Item < ApplicationRecord
   validates :price,
     numericality: {
     greater_than_or_equal_to: 300,
-    less_than: 9999999
+    less_than: 10000000
     }
-  validates :image, presence: true
-  validates :name, :product_text, presence: true
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 } 
-  validates :delivery_fee_id, numericality: { other_than: 1 } 
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :delivery_day_id, numericality: { other_than: 1 }
 
+  with_options numericality: { other_than:1} do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :delivery_day_id
+
+  end
+  validates :image, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -18,31 +18,31 @@ RSpec.describe Item, type: :model do
       expect(@item).to be_valid
     end
     it 'カテゴリー情報に1以外を選択すれば登録できる' do
-      @item.category_id = "2"
+      @item.category_id = 2
       expect(@item).to be_valid
     end
     it '商品状態の情報に1以外を選択すれば登録できる' do
-      @item.condition_id = "2"
+      @item.condition_id = 2
       expect(@item).to be_valid
     end
     it '配送料の負担情報に1以外を選択すれば登録できる' do
-      @item.delivery_fee_id = "2"
+      @item.delivery_fee_id = 2
       expect(@item).to be_valid
     end
     it '発送元の地域の情報に1以外を選択すれば登録できる' do
-      @item.prefecture_id = "2"
+      @item.prefecture_id = 2
       expect(@item).to be_valid
     end
     it '発送までの日数の情報に1以外を選択すれば登録できる' do
-      @item.delivery_day_id = "2"
+      @item.delivery_day_id = 2
       expect(@item).to be_valid
     end
     it '販売価格を¥300~¥9999999の間で入力されていれば登録できる' do
-      @item.price = "1000"
+      @item.price = 1000
       expect(@item).to be_valid
     end
     it '販売価格が半角数字で入力されていれば登録できる' do
-      @item.price = "1000"
+      @item.price = 1000
       expect(@item).to be_valid
     end
   end
@@ -65,37 +65,43 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Product text can't be blank")
     end
     it 'カテゴリー情報が1では登録できない' do
-      @item.category_id = '1'
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Category must be other than 1")
     end
     it '商品状態の情報が1では登録できない' do
-      @item.condition_id = '1'
+      @item.condition_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition must be other than 1")
     end
     it '配送料の負担情報が1では登録できない' do
-      @item.delivery_fee_id = '1'
+      @item.delivery_fee_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery fee must be other than 1")
     end
     it '発送元の地域の情報が1では登録できない' do
-      @item.prefecture_id = '1'
+      @item.prefecture_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
     end
     it '発送までの日数の情報が1では登録できない' do
-      @item.delivery_day_id = '1'
+      @item.delivery_day_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Delivery day must be other than 1")
     end
-    it '販売価格が¥300未満、¥9999999を超えると登録できない' do
-      @item.price = '100'
+    it '販売価格が¥300未満だと登録できない' do
+      @item.price = 100
       @item.valid?
       expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
     end
+
+    it '販売価格が¥9999999を超えると登録できない' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than 9999999")
+    end
     it '販売価格は半角数字以外では登録できない' do
-      @item.price = '１０００'
+      @item.price = "１０００"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is not a number")
     end
