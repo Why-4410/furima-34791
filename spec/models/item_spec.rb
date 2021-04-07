5aa9e34fbdeb2fb8fa47cbb118f0f2bec7d1188d
Why-4410 <yssy4410@gmail.com>
2021-04-07 12:35:36 +0900

    レビューの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -109,13 +109,13 @@ RSpec.describe Item, type: :model do
     it '販売価格は半角英数混合では登録できない' do
       @item.price = '1a1a'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price is not a number", "Price is not a number")
+      expect(@item.errors.full_messages).to include("Price is not a number")
     end
 
     it '半角英語だけでは登録できない' do
       @item.price = 'aaa'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price is not a number", "Price is not a number")
+      expect(@item.errors.full_messages).to include("Price is not a number")
     end
   end
 end
